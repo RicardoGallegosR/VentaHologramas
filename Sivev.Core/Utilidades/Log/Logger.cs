f7b073e437f738c08317af651f2eccc5277a7191
Ricardo Gallegos <ricardoipn9@gmail.com>
2025-08-11 13:22:23 -0600

    Se modifica el ignore

---
+++ Sivev.Core/Utilidades/Log/Logger.cs
@@ -0,0 +1,22 @@
+﻿namespace Sivev.Core.Utilidades.Log {
+    public class Logger : IDisposable {
+        private readonly string _logFilePath;
+        private readonly StreamWriter _writer;
+
+        public Logger(string moduleName) {
+            var logDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LogProcesos");
+            Directory.CreateDirectory(logDir);
+            _logFilePath = Path.Combine(logDir, $"{moduleName}_{DateTime.Now:yyyyMMdd_HHmmss}.log");
+            _writer = new StreamWriter(_logFilePath, true) { AutoFlush = true };
+        }
+
+        public void Log(string library, string method, string message) {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
+            _writer.WriteLine($"{timestamp} | {library} | {method} | {message}");
+        }
+
+        public void Dispose() {
+            _writer?.Dispose();
+        }
+    }
+}
